073e1ae2330da95fa7fe1152e29848965493afa4
dberre <74907040+dberre@users.noreply.github.com>
2024-07-02 21:30:08 +0200

    Move the code into a class

--- include/Ads1115.h
+++ include/Ads1115.h
@@ -4,11 +4,31 @@
 #define _ADS1115_H
 
 class Ads1115Sensor {
-    
+private:    
+    uint _numberOfSamples = 0;
+    uint _adcChannel = 0;
+    float _convertedValue;
+
+    byte _alertInterruptPin = 33;
+
+    static Ads1115Sensor *_defaultInstance;
+
+    static ADS1115 *_board;
+
+    static SemaphoreHandle_t xBinarySemaphore;
+
+    static TaskHandle_t xTaskToNotify;
+
+public:
+    Ads1115Sensor();
+
+    static Ads1115Sensor *defaultInstance();
+
+    float readRmsVoltage(uint channel, uint numberOfSamples);
+
+private:
+    static void ISR_RMScallback();
+    static void ISR_RMSprocessing(void * parameter);
 };
-
-
-float readRmsVoltage(uint channel, uint numberOfSamples);
-
 
 #endif
--- src/Ads1115.cpp
+++ src/Ads1115.cpp
@@ -2,67 +2,39 @@
 
 #include "Ads1115.h"
 
-static uint _numberOfSamples = 0;
-static uint _adcChannel = 0;
-static float _convertedValue;
+Ads1115Sensor *Ads1115Sensor::_defaultInstance = 0;
 
-static ADS1115 ADS(0x48);
-static SemaphoreHandle_t xBinarySemaphore;
+ADS1115 *Ads1115Sensor::_board = 0;
 
-static TaskHandle_t xTaskToNotify = NULL;
+SemaphoreHandle_t Ads1115Sensor::xBinarySemaphore = 0;
 
-static byte _alertInterruptPin = 33;
+TaskHandle_t Ads1115Sensor::xTaskToNotify = 0;
 
-// The ISR routine called when the ADC conversion is ready
-void ISR_RMScallback() {
-  ets_printf("IRS_RMS\n");
-  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  xSemaphoreGiveFromISR(xBinarySemaphore, &xHigherPriorityTaskWoken);
-  portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
-}
+Ads1115Sensor::Ads1115Sensor() {
+  _board = new ADS1115(0x48);
 
-// The task which performs the RMS calculation
-void ISR_RMSprocessing(void * parameter)
-{
-  uint32_t sumAdc = 0;
-  uint32_t sumSquareAdc = 0;
-  uint32_t samplesCount = 0;
-
-  for(;;) {
-    // block and wait for an interrupt event
-    if (xSemaphoreTake(xBinarySemaphore, pdMS_TO_TICKS(10000)) == pdFALSE) break;
-    // int adc = ADS.getValue();
-    // sumAdc += adc;
-    // sumSquareAdc += (adc * adc);
-    samplesCount++;
-    ets_printf("loop %d\n", samplesCount);
-
-    // if the expected number of samples is reached then exit
-    if (samplesCount == _numberOfSamples) {
-      float rmsVoltage = (float)(ADS.getMaxVoltage() / (1 << 16)) * sqrt(((double)sumSquareAdc / _numberOfSamples) - pow((double)sumAdc / _numberOfSamples, 2));
-      ets_printf("%d;%d;%d;%d\n", _numberOfSamples, sumAdc, sumSquareAdc, (int32_t)(rmsVoltage * 1000));
-      break;
-    }
-  }
-
-  xTaskNotify(xTaskToNotify, 0, eNoAction);
-  vTaskDelete(NULL);
-}
-
-float readRmsVoltage(uint channel, uint numberOfSamples) {
-  _adcChannel = channel;
-  _numberOfSamples = numberOfSamples;
+  Ads1115Sensor::xBinarySemaphore = xSemaphoreCreateBinary();
 
   pinMode(_alertInterruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(_alertInterruptPin), ISR_RMScallback, FALLING);
+}
 
-  xBinarySemaphore = xSemaphoreCreateBinary();
+Ads1115Sensor *Ads1115Sensor::defaultInstance() {
+  if (_defaultInstance == 0) {
+    _defaultInstance = new Ads1115Sensor();
+  }
+  return _defaultInstance;
+}
+
+float Ads1115Sensor::readRmsVoltage(uint channel, uint numberOfSamples) {
+  _adcChannel = channel;
+  _numberOfSamples = numberOfSamples;
 
   xTaskCreate(
       ISR_RMSprocessing,       /* Task function. */
       "ISR_RMSprocessing",     /* name of task. */
       configMINIMAL_STACK_SIZE + 100, /* Stack size of task */
-      NULL,                    /* parameter of the task */
+      (void *)_numberOfSamples,                    /* parameter of the task */
       4,                       /* priority of the task TODO */
       NULL);
 
@@ -92,9 +64,45 @@ float readRmsVoltage(uint channel, uint numberOfSamples) {
   uint32_t ulNotifiedValue = ulTaskNotifyTake(pdFALSE, portMAX_DELAY);
   ets_printf("return:%d\n", ulNotifiedValue);
   // ADS.reset();
-  vSemaphoreDelete(xBinarySemaphore);
 
-  return _convertedValue;
+  return ulNotifiedValue / 1000.0;
 }
 
+// The ISR routine called when the ADC conversion is ready
+void Ads1115Sensor::ISR_RMScallback() {
+  ets_printf("IRS_RMS\n");
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  xSemaphoreGiveFromISR(xBinarySemaphore, &xHigherPriorityTaskWoken);
+  portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+// The task which performs the RMS calculation
+void Ads1115Sensor::ISR_RMSprocessing(void * parameter) {
+  uint32_t numberOfSamples = (uint32_t)parameter;
   
+  uint32_t sumAdc = 0;
+  uint32_t sumSquareAdc = 0;
+  uint32_t samplesCount = 0;
+  float rmsVoltage = 0.0;
+
+  for(;;) {
+    // block and wait for an interrupt event
+    if (xSemaphoreTake(xBinarySemaphore, pdMS_TO_TICKS(10000)) == pdFALSE) break;
+    // int adc = ADS.getValue();
+    int adc = (samplesCount == 1 ? 4000 : 2000); // FIXME
+    sumAdc += adc;
+    sumSquareAdc += (adc * adc);
+    samplesCount++;
+    ets_printf("loop %d\n", samplesCount);
+
+    // if the expected number of samples is reached then exit
+    if (samplesCount == numberOfSamples) {
+      rmsVoltage = (_board->getMaxVoltage() / (1 << 16)) * sqrt(((double)sumSquareAdc / numberOfSamples) - pow((double)sumAdc / numberOfSamples, 2));
+      ets_printf("%d;%d;%d;%d\n", numberOfSamples, sumAdc, sumSquareAdc, (int32_t)(rmsVoltage * 1000));
+      break;
+    }
+  }
+
+  xTaskNotify(Ads1115Sensor::xTaskToNotify, (uint32_t)(rmsVoltage * 1000), eSetValueWithOverwrite);
+  vTaskDelete(NULL);
+}
